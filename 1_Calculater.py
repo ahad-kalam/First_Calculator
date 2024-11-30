877632df9a7eae16abe7ea3cd418975fec2ef179
ahad-kalam <abdulahadkalam37@gmail.com>
2024-11-30 22:20:02 +0530

    Update 1_Calculater.py

--- 1_Calculater.py
+++ 1_Calculater.py
@@ -1,22 +1,22 @@
-a = int(input('Enter your 1st value : '))
+num1 = int(input('Enter your 1st value : '))
 c = input('Enter your opreator like(+,-,*,/) : ')
 b = int(input('Enter your 2nd value : '))
 
  
 if c == '+':
-    print(f'the sum of {a} and {b} is : {a + b}')
+    print(f'the sum of {num1} and {b} is : {num1 + b}')
 elif c == "-":
-    print(f'the sub of {a} and {b} is : {a - b}')
+    print(f'the sub of {num1} and {b} is : {num1 - b}')
 elif c == "*":
-    print(f'the multiplication of {a} and {b} is : {a * b}')
+    print(f'the multiplication of {num1} and {b} is : {num1 * b}')
 elif c == "**":
-    print(f'the multiplication of {a} and {b} is : {a ** b}')
+    print(f'the multiplication of {num1} and {b} is : {num1 ** b}')
 elif c == '/':      
-    print(f'the divide of {a} and {b} is : {a/b}')
+    print(f'the divide of {num1} and {b} is : {num1/b}')
 elif c == '//':
-    print(f'the divide of {a} and {b} is : {a//b}')
+    print(f'the divide of {num1} and {b} is : {num1//b}')
 elif c == "%":
-    print(f'the reminder of {a} and {b} is : {a%b}')
+    print(f'the reminder of {num1} and {b} is : {num1%b}')
 else :
     print('invalib operator...!')
 
